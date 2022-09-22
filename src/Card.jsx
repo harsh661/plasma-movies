4d6cbbea3b55c812d2834f2283cda0195fbd1cc4
harsh661 <73983648+harsh661@users.noreply.github.com>
2022-09-22 20:48:44 +0530

    fixed error message

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,29 +10,28 @@ export default function Card(props) {
       .then((data) => setMovie(data));
   }, [props.willShow]);
 
-  return (
-    <>
-      {movie.Error !== "Movie not found" && movie.Title !== undefined ?
-        <div className="card">
-          <div className="poster">
-            <img src={movie.Poster} alt={movie.Title} />
+  console.log(movie)
+
+  if(movie.Error !== "Movie not found"  && movie.Title !== undefined){
+    return (
+          <div className="card">
+            <div className="poster">
+              <img src={movie.Poster} alt={movie.Title} />
+            </div>
+            <div className="about">
+              <span className="rating">
+                <FaStar style={{color:'#f7b900'}}/>
+                <span>{movie.imdbRating}</span>
+              </span>
+              <span style={{padding: '10px 0'}}>Language: {movie.Language}</span>
+              <h2 className="title">{movie.Title} {`(${movie.Year})`}</h2>
+              <h4>{movie.Genre}</h4>
+              <p className="story">{movie.Plot}</p>
+              <span className="date">
+                  Released on: <b>{movie.Released}</b>
+              </span>
+            </div>
           </div>
-          <div className="about">
-            <span className="rating">
-              <FaStar style={{color:'#f7b900'}}/>
-              <span>{movie.imdbRating}</span>
-            </span>
-            <span style={{padding: '10px 0'}}>Language: {movie.Language}</span>
-            <h2 className="title">{movie.Title} {`(${movie.Year})`}</h2>
-            <h4>{movie.Genre}</h4>
-            <p className="story">{movie.Plot}</p>
-            <span className="date">
-                Released on: <b>{movie.Released}</b>
-            </span>
-          </div>
-        </div>: <h1 className="error-msg">{props.msg}</h1>
-      }
-      
-    </>
-  );
+    )
+  }
 }
